d714b1475b5c32df1727be06a851e9f5c253c638
brunopenso <viperp@gmail.com>
2021-04-18 11:12:25 -0300

    refactor

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -54,76 +54,67 @@ describe('invalid array', () => {
   })
 })
 
-test('validate object attribute pattern definition', function () {
-  try {
-    validateRoute([{ pattern: 1, method: 2, exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('Pattern should be a string in the pattern /path/path2/:param')
-  }
-
-  try {
-    validateRoute([{ pattern: 'path', method: 2, exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('Pattern should be a string in the pattern /path/path2/:param')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path{', method: 2, exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('Pattern should be a string in the pattern /path/path2/:param')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path}', method: 2, exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('Pattern should be a string in the pattern /path/path2/:param')
-  }
+describe('validate object attribute pattern definition', () => {
+  test('pattern must be string', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: 1, method: 2, exec: 3 }])
+    }).rejects.toThrow('Pattern should be a string in the pattern /path/path2/:param')
+  })
+  test('string pattern invalid', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: 'path', method: 2, exec: 3 }])
+    }).rejects.toThrow('Pattern should be a string in the pattern /path/path2/:param')
+  })
+  test('string pattern invalid chars {', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path{', method: 2, exec: 3 }])
+    }).rejects.toThrow('Pattern should be a string in the pattern /path/path2/:param')
+  })
+  test('string pattern invalid chars }', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path}', method: 2, exec: 3 }])
+    }).rejects.toThrow('Pattern should be a string in the pattern /path/path2/:param')
+  })
 })
 
-test('validate object attribute method definition', function () {
-  try {
-    validateRoute([{ pattern: '/path', method: 2, exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path', method: 'x', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path', method: 'get', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path', method: 'post', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path', method: 'put', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
-
-  try {
-    validateRoute([{ pattern: '/path', method: 'delete', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('The only methods supported are GET,POST,PUT,DELETE')
-  }
+describe('validate object attribute method definition', () => {
+  const errorMessage = 'The only methods supported are GET,POST,PUT,DELETE'
+  test('method as number', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 2, exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
+  test('method with x', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 'x', exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
+  test('methd lowercase get', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 'get', exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
+  test('methd lowercase post', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 'post', exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
+  test('methd lowercase put', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 'put', exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
+  test('methd lowercase delete', async () => {
+    await expect(async () => {
+      validateRoute([{ pattern: '/path', method: 'delete', exec: 3 }])
+    }).rejects.toThrow(errorMessage)
+  })
 })
 
-test('validate object attribute exec definition', function () {
-  try {
+test('validate object attribute exec definition', async () => {
+  await expect(async () => {
     validateRoute([{ pattern: '/path', method: 'GET', exec: 3 }])
-  } catch (err) {
-    expect(err.message).toBe('Exec should be a function')
-  }
+  }).rejects.toThrow('Exec should be a function')
 })
 
 test('success', function () {
